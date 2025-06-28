e6b3b3aeca4db6e4c2a2939da56d9ba3fdd7d82c
Alisher Nildibayev <alisher.nil@gmail.com>
2025-06-29 00:40:10 +0500

    refactor the asteroid split mechanics

--- asteroid.py
+++ asteroid.py
@@ -9,27 +9,25 @@ ASTEROID_WIDTH = 2
 
 
 class Asteroid(CircleShape):
-    def __init__(self, x, y, radius):
+    def __init__(self, x, y, radius) -> None:
         super().__init__(x, y, radius)
 
-    def draw(self, screen):
+    def draw(self, screen) -> None:
         pygame.draw.circle(screen, "white", self.position, self.radius)
 
-    def update(self, dt):
+    def update(self, dt) -> None:
         self.position += self.velocity * dt
 
-    def split(self):
+    def split(self) -> None:
         self.kill()
         if self.radius <= ASTEROID_MIN_RADIUS:
             return
         angle = random.uniform(20, 50)
-        child_vector1 = self.velocity.rotate(angle)
-        child_vector2 = self.velocity.rotate(-angle)
-
         child_radius = self.radius - ASTEROID_MIN_RADIUS
+        self.spawn_child(child_radius, angle)
+        self.spawn_child(child_radius, -angle)
 
-        child1 = Asteroid(self.position.x, self.position.y, child_radius)
-        child2 = Asteroid(self.position.x, self.position.y, child_radius)
-
-        child1.velocity = child_vector1 * ASTEROID_ACCEL_RATE
-        child2.velocity = child_vector2 * ASTEROID_ACCEL_RATE
+    def spawn_child(self, radius: float, escape_angle: float) -> None:
+        child = Asteroid(self.position.x, self.position.y, radius)
+        child_vector = self.velocity.rotate(escape_angle)
+        child.velocity = child_vector * ASTEROID_ACCEL_RATE
